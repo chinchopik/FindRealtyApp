15df4f895da14fc0721e6719c8ca5692801f281d
chinchopik <ishutkin0143@gmail.com>
2023-06-02 00:35:31 +0300

    Добавьте файлы проекта.

---
+++ FindRealtyApp/Repositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using FindRealtyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindRealtyApp.Repositories
+{
+    class UserRepository : RepositoryBase, IUserRepository
+    {
+        public bool AuthenticateUser(string username, string password)
+        {
+            bool validUser;
+            using (var connection = GetConnection())
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "select * from [User] where login=@username and password=@password";
+                command.Parameters.Add("@username", System.Data.SqlDbType.NVarChar).Value = username;
+                command.Parameters.Add("@password", System.Data.SqlDbType.NVarChar).Value = password;
+                validUser = command.ExecuteScalar() == null ? false : true;
+            }
+            return validUser;
+        }
+    }
+}
---
+++ FindRealtyApp/ViewModel/LoginViewModel.cs
@@ -0,0 +1,64 @@
+﻿using FindRealtyApp.Models;
+using FindRealtyApp.Repositories;
+using FindRealtyApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FindRealtyApp.ViewModel
+{
+    class LoginViewModel : BaseModel
+    {
+        private string _password;
+        private string _username;
+
+        private IUserRepository userRepository;
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged("Username");
+            }
+        }
+
+        public ViewModelCommand Auth { get; private set; }
+        public LoginViewModel()
+        {
+            userRepository = new UserRepository();
+            Auth = new ViewModelCommand(Authenticate);
+        }
+
+        private void Authenticate()
+        {
+            var isValidUser = userRepository.AuthenticateUser(Username, Password);
+            if (isValidUser)
+            {
+                Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(Username), null);
+                MenuWindow menuWindow = new MenuWindow();
+                Application.Current.MainWindow.Close();
+                menuWindow.Show();
+
+            }
+            else
+                MessageBox.Show("freak-vonuchka");
+        }
+    }
+}
